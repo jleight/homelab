64b426e01c5a76fdbbd43a75987cbe371a5fb892
Jonathon Leight <jonathon.leight@jleight.com>
2025-10-25 16:40:08 -0400

    Add tailscale app connector

--- lib/terraform/modules/k8s/ingress/main_tailscale_connector.tf
+++ lib/terraform/modules/k8s/ingress/main_tailscale_connector.tf
@@ -1,3 +1,33 @@
+resource "kubectl_manifest" "tailscale_exit" {
+  count = local.tailscale_enabled ? 1 : 0
+
+  yaml_body = yamlencode({
+    apiVersion = "tailscale.com/v1alpha1"
+    kind       = "Connector"
+
+    metadata = {
+      name = "exit-${local.stack}-${local.environment}"
+    }
+
+    spec = {
+      hostname = "tailscale-exit-${local.stack}-${local.environment}"
+      exitNode = true
+
+      subnetRouter = {
+        advertiseRoutes = [
+          module.ipam.lan.v4_cidr,
+          module.ipam.lan.v6_cidr,
+          module.ipam.resources.pods,
+          module.ipam.resources.services,
+          module.ipam.resources.load_balancers,
+        ]
+      }
+    }
+  })
+
+  depends_on = [helm_release.tailscale]
+}
+
 resource "kubectl_manifest" "tailscale_connector" {
   count = local.tailscale_enabled ? 1 : 0
 
@@ -11,17 +41,8 @@ resource "kubectl_manifest" "tailscale_connector" {
 
     spec = {
       hostname = "tailscale-connector-${local.stack}-${local.environment}"
-      exitNode = true
 
-      subnetRouter = {
-        advertiseRoutes = [
-          module.ipam.lan.v4_cidr,
-          module.ipam.lan.v6_cidr,
-          module.ipam.resources.pods,
-          module.ipam.resources.services,
-          module.ipam.resources.load_balancers,
-        ]
-      }
+      appConnector = {}
     }
   })
 
